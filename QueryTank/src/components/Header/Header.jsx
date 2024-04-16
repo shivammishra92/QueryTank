46271973905a9a01ea3e38e04c127f26caf95bd6
Shivam Mishra <shivam552mishra@gmail.com>
2024-04-17 02:34:29 +0530

    first commit

---
+++ QueryTank/src/components/Header/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './css/Header.css'
+
+function Header() {
+  return (
+    <header>
+
+        <div className='header-container'>
+          <div className="header-left">
+
+          </div>
+          <div className="header-middle">
+
+          </div>
+          <div className="header-right">
+
+          </div>
+        </div>
+        
+    </header>
+    
+  )
+}
+
+export default Header
